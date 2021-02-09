9ef17eb079b42b790bda753a7d4cede516bb467c
MateMarzec <marzec.mate@gmail.com>
2021-02-09 00:03:30 +0000

    Step 1 - Models(In progress)
    -General filed was changed
    -Some basic models added

---
+++ EventsPlusSystem/EventsPlusSystem/Models/EventParticipant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsPlusSystem.Models
+{
+    public abstract class EventParticipant
+    {
+    }
+}
